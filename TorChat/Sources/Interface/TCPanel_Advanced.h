189f3eaab02f27d0396c677793b412a39e2fb128
Julien-Pierre Avérous <j.averous@sourcemac.com>
2013-07-31 22:09:10 +0200

    - Restructurate assistant code (split in multiple files / multiple xib).

---
+++ TorChat/Sources/Interface/TCPanel_Advanced.h
@@ -0,0 +1,21 @@
+//
+//  TCPanel_Advanced.h
+//  TorChat
+//
+//  Created by Julien-Pierre Avérous on 31/07/13.
+//  Copyright (c) 2013 SourceMac. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "TCPanel.h"
+
+
+/*
+** TCPanel_Advanced
+*/
+#pragma mark - TCPanel_Advanced
+
+@interface TCPanel_Advanced : NSViewController <TCAssistantPanel>
+
+@end
---
+++ TorChat/Sources/Interface/TCPanel_Basic.h
@@ -0,0 +1,21 @@
+//
+//  TCPanel_Basic.h
+//  TorChat
+//
+//  Created by Julien-Pierre Avérous on 31/07/13.
+//  Copyright (c) 2013 SourceMac. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "TCPanel.h"
+
+
+/*
+** TCPanel_Basic
+*/
+#pragma mark - TCPanel_Basic
+
+@interface TCPanel_Basic : NSViewController <TCAssistantPanel>
+
+@end
