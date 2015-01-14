f302e89f0f5def547fab8a541cd12b2f1169655f
Julien-Pierre Avérous <github@sourcemac.com>
2015-01-15 00:09:45 +0100

    - Restructure project hierarchy. - Add interface config protocol for UI related settings. - Prepare preferences window to handle new locations mecanism.

---
+++ TorChat/TorChat/TCPrefView.m
@@ -0,0 +1,35 @@
+//
+//  TCPrefsView.m
+//  TorChat
+//
+//  Created by Julien-Pierre Avérous on 14/01/2015.
+//  Copyright (c) 2015 SourceMac. All rights reserved.
+//
+
+#import "TCPrefView.h"
+
+
+/*
+** TCPrefView
+*/
+#pragma mark - TCPrefView
+
+@implementation TCPrefView
+
+
+/*
+** TCPrefView - Config
+*/
+#pragma mark - TCPrefView - Config
+
+- (void)loadConfig
+{
+	// Must be redefined
+}
+
+- (void)saveConfig
+{
+	// Must be redefined
+}
+
+@end
---
+++ TorChat/TorChat/TCPrefView_General.h
@@ -0,0 +1,19 @@
+//
+//  TCPrefView_General.h
+//  TorChat
+//
+//  Created by Julien-Pierre Avérous on 14/01/2015.
+//  Copyright (c) 2015 SourceMac. All rights reserved.
+//
+
+#import "TCPrefView.h"
+
+
+/*
+** TCPrefView_General
+*/
+#pragma mark - TCPrefView_General
+
+@interface TCPrefView_General : TCPrefView
+
+@end
