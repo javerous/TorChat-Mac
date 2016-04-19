ffc1dbc13dd5ad7c6c25d6527c2865b1880e3d99
Julien-Pierre Avérous <github@sourcemac.com>
2016-04-19 21:25:05 +0200

    - Add GPL header.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 
+#
+#  build.sh
+#
+#  Copyright 2016 Avérous Julien-Pierre
+#
+#  This file is part of TorChat.
+#
+#  TorChat is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  TorChat is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with TorChat.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
 # Get base directory
 base=$( dirname "$0" )
 base=$( cd "${base}/../" ; pwd -P )
